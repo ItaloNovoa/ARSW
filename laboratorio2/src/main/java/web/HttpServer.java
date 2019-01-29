0419b9961b2c2ad415159c1c646911163882d0ea
ItaloNovoa <italolo97@hotmail.com>
2019-01-28 22:16:32 -0500

    errores http socket

--- laboratorio2/src/main/java/web/HttpServer.java
+++ laboratorio2/src/main/java/web/HttpServer.java
@@ -23,16 +23,42 @@ public class HttpServer {
 			PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
 			BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
 			String inputLine, outputLine;
+			int j =1;
+			String get="";
 			while ((inputLine = in .readLine()) != null) {
+				if(j==1) {
+					get=inputLine.substring(5,(inputLine.length()-" HTTP/1.1".length()));
+					System.out.println(get);
+					j++;
+				}
 				System.out.println("Received: " + inputLine);
 				if (! in .ready()) {
 					break;
 				}
 			}
+			BufferedReader b=null;
+			
+			try {
+				FileReader f = new FileReader(System.getProperty("C:\\Users\\ItaloPC\\Desktop\\ARSW\\laboratorio2\\")+get);
+	   	     	b = new BufferedReader(f);
+			} catch (Exception e) {
+				System.out.println("no se pudo");
+			}
+			
+			
+			while ((inputLine = b.readLine()) != null) {				
+				out.println(inputLine);
+				if (! in .ready()) {
+					break;
+				}
+			}
+			
 			outputLine = "<!DOCTYPE html>" + "<html>" + "<head>" + "<meta charset=\"UTF-8\">" + "<title>Title of the document</title>\n" + "</head>" + "<body>" + "My Web Site" + "</body>" + "</html>" + inputLine;
 			out.println(outputLine);
 			out.close(); in .close();
 			clientSocket.close();
+			
+			
 		}
 	
 		//serverSocket.close();
